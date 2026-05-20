fcd23c9998d904ea4362c291ce694896410f61cd
Tisha Dey <tishadey2025@outlook.com>
2026-05-21 02:00:53 +0600

    :sparkles: implement backend API routes

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d2ts7wd.mongodb.net/?appName=Cluster0`;
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d2ts7wd.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -25,10 +24,39 @@ async function run() {
   try {
     await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!",
+      "Pinged your deployment. You successfully connected to MongoDB ✅",
     );
+    const db = client.db("study-nook");
+    const roomCollection = db.collection("rooms");
+
+    app.post("/add-room", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await roomCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/rooms", async (req, res) => {
+      const result = await roomCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await roomCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.delete("/api/rooms/:id", async (req, res)=>{
+        const id = req.params.id;
+        console.log(id);
+        const query = {_id: new ObjectId(id)};
+        const result = await roomCollection.deleteOne(query);
+        res.send(result);
+    })
   } finally {
-  
   }
 }
 run().catch(console.dir);
