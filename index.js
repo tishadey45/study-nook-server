d2c7c79f3fe5a65475053bac4a8a650ec45fc35f
Tisha Dey <tishadey2025@outlook.com>
2026-05-24 00:37:56 +0600

    :recycle: created jwt middleware

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+//  middleware
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d2ts7wd.mongodb.net/?appName=Cluster0`;
@@ -20,6 +23,23 @@ const client = new MongoClient(uri, {
   },
 });
 
+// verify token
+function verifyToken(req, res,next){
+    const token = req.cookies.token;
+    console.log(token);
+    if(!token){
+        return res.status(401).send({error:true,message: 'Unauthorized access'});
+
+    }
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,function(err,decoded){
+        if(err){
+            return res.status(403).send({error:true,message:"Forbidden access"});
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
   try {
     await client.db("admin").command({ ping: 1 });
@@ -28,6 +48,22 @@ async function run() {
     );
     const db = client.db("study-nook");
     const roomCollection = db.collection("rooms");
+    const orderCollection = db.collection("orders");
+
+    //   JWT
+    app.post("/jwt", (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "24h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
 
     app.post("/add-room", async (req, res) => {
       const user = req.body;
@@ -49,31 +85,65 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/api/rooms/:id", async (req, res)=>{
-        const id = req.params.id;
-        console.log(id);
-        const query = {_id: new ObjectId(id)};
-        const result = await roomCollection.deleteOne(query);
-        res.send(result);
-    })
+    app.delete("/api/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await roomCollection.deleteOne(query);
+      res.send(result);
+    });
 
-    app.put("/api/rooms/:id", async (req,res)=>{
-        const id = req.params.id;
-        console.log(id);
-        const filter = {_id: new ObjectId(id)};
-        const upDatedRoom = req.body;
-        const upDateDoc = {
-            $set:{
-                room_name: upDatedRoom.room_name,
-                floor: upDatedRoom.floor,
-                capacity: upDatedRoom.capacity,
-                hourly_rate: upDatedRoom.hourly_rate,
-            }
-        }
-        const options = {upsert: true};
-        const result =await roomCollection.updateOne(filter,upDateDoc,options);
-        res.send(result);
-    })
+    app.put("/api/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const upDatedRoom = req.body;
+      const upDateDoc = {
+        $set: {
+          room_name: upDatedRoom.room_name,
+          floor: upDatedRoom.floor,
+          capacity: upDatedRoom.capacity,
+          hourly_rate: upDatedRoom.hourly_rate,
+        },
+      };
+      const options = { upsert: true };
+      const result = await roomCollection.updateOne(filter, upDateDoc, options);
+      res.send(result);
+    });
+
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      console.log(order);
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+
+    app.get("/my-bookings/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const query = { "buyer.email": email };
+      const result = await orderCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.patch("/api/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      const updateDoc = {
+        $set: {
+          status: "cancelled",
+        },
+      };
+      const options = { upsert: true };
+      const result = await orderCollection.updateOne(
+        filter,
+        updateDoc,
+        options,
+      );
+      res.send(result);
+    });
   } finally {
   }
 }
